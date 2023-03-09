98e7f8039f8013bb03d1f2e7caded854ec1b87f6
John Carlson <johncarlson21@gmail.com>
2023-03-09 14:14:39 -0600

    update version number

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "2.1.3"
+  #define SHORT_BUILD_VERSION "2.1.3.1"
 #endif
 
 /**
@@ -66,7 +66,7 @@
  * (Other behaviors are given by the firmware version and capabilities report.)
  */
 #ifndef PROTOCOL_VERSION
-  #define PROTOCOL_VERSION "2.1.3"
+  #define PROTOCOL_VERSION "2.1.3.1"
 #endif
 
 /**
